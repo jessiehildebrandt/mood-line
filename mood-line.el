82856d918638abef94c07d10a6db0468d2ab5c62
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-30 05:37:26 -0400

    Increment minor version number

--- mood-line.el
+++ mood-line.el
@@ -3,7 +3,7 @@
 ;; Author: Jessie Hildebrandt <jessieh.net>
 ;; Homepage: https://gitlab.com/jessieh/mood-line
 ;; Keywords: mode-line faces
-;; Version: 1.1.2
+;; Version: 1.2.0
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This file is not part of GNU Emacs.
